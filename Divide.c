09c5ff4a6d258e5d34102dd80cd7469a174e8177
aishuv582 <aishuv830@gmail.com>
2026-01-23 22:14:20 +0530

    Divide.c

---
+++ Divide.c
@@ -0,0 +1,20 @@
+/*
+Program Name : Divide.c
+Author       : Aishwarya V
+Description  : Program about arithmetic operations
+*/
+
+
+#include<stdio.h>				//header file
+
+int main ()			
+
+{
+	int x;						//declared variables x
+	printf("Enter a number: ");
+	scanf("%d",&x);
+	x=x/6;						//divide the user input by 6
+	printf("Result=%d",x);
+	
+	return 0;
+}
