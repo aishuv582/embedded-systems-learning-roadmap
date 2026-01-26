55f280b57877ffa2ce4a63d20cb2aae24c1fafc8
aishuv582 <aishuv830@gmail.com>
2026-01-26 22:12:03 +0530

    ondigitwith3digitnum.c

---
+++ ondigitwith3digitnum.c
@@ -0,0 +1,30 @@
+/*
+Program Name : onesdigitwith3digitnum.c
+Author       : Aishwarya V
+Description  : Program about printing one's digit in a given 3 digit number
+*/
+#include<stdio.h>
+
+int main()
+
+{
+	int x;
+	printf("Enter a three digit no: ");		//get a three digit number from user
+	scanf("%d",&x);
+	if(x>=100 && x<=999)						//condition for a three digit number
+	{ 
+	
+
+	printf("one's digit = %d", x%10);			//print ones digit
+	
+	}
+	else
+{
+		
+    
+        printf("Please enter a valid three-digit number");			//other 3 digit no any digit more than 3 print no as invalid
+    
+	}
+	
+	return 0;
+}
