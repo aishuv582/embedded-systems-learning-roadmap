848e4e42e96f9e7537407ccfbd1efcb2aae44e9a
aishuv582 <aishuv830@gmail.com>
2026-01-26 22:54:42 +0530

    tensdigitusing3digit.c

---
+++ tensdigit using 3 digit num.c
@@ -0,0 +1,30 @@
+/*
+Program Name : tensdigitusing3digit.c
+Author       : Aishwarya V
+Description  : Program about printing ten's digit in a given 3 digit number
+*/
+#include<stdio.h>
+
+int main()
+
+{
+	int x;
+	printf("Enter a three digit no: ");		//get a three digit number from user
+	scanf("%d",&x);
+	if(x>=100 && x<=999)						//condition for a three digit number
+	{ 
+	
+	
+	printf("Ten's digit = %d", (x%100)/10);			//print tens digit
+	
+	}
+	else
+{
+		
+    
+        printf("Please enter a valid three-digit number");			//other 3 digit no any digit more than 3 print no as invalid
+    
+	}
+	
+	return 0;
+}
