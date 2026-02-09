d010dbfcfdbf03e8402e665d5f2c3b3cb1aa895f
aishuv582 <aishuv830@gmail.com>
2026-02-09 08:10:05 +0530

    checkifgivennumlessthan50.c

---
+++ checkifgivennumlessthan50.c
@@ -0,0 +1,23 @@
+/*
+Program Name : checkifgivennumlessthan50.c
+Author       : Aishwarya V
+Description  : Program about checkifgivennumlessthan50
+*/
+#include<stdio.h>			//header file
+
+int main()					//main function
+
+{
+	int x;					//declared x
+	printf("Enter a num : ");			//getting a user input
+	scanf("%d",&x);
+	if(x<50)					//condition for num<50
+	{
+		printf("1");
+		
+	}
+	else{
+		printf("0");
+	}
+	return 0;
+}
