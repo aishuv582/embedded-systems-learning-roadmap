69310374fdc5ec5fd7a9b3e8c4049279719884ac
aishuv582 <aishuv830@gmail.com>
2026-02-11 08:14:53 +0530

    checkifdigit0anddigit1areidenticalornotina2digitnum

---
+++ checkifdigit0anddigit1areidenticalornotina2digitnum.c
@@ -0,0 +1,22 @@
+/*
+Program Name : checkifdigit0anddigit1areidenticalornotina2digitnum.c
+Author       : Aishwarya V
+Description  : Program about check if digit 0 and digit 1 are identical or not
+*/
+
+
+
+
+#include <stdio.h>			//header file
+
+int main()					// main function	
+{
+    int x;					//declared variable x
+
+    printf("Enter a 2-digit number: ");			//getting a 2 digit input from the user
+    scanf("%d", &x);							
+
+    printf("%d", (x/10) == (x%10));			//check if digit 0 and digit 1 are identical or not
+
+    return 0;
+}
