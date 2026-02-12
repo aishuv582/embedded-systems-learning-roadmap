b6bf83ab3f148adf9f0e66e24e1f6218bf5bbf45
aishuv582 <aishuv830@gmail.com>
2026-02-12 07:50:35 +0530

    checkifdigitlessthandigit1ina2digitnum.c

---
+++ checkifdigitlessthandigit1ina2digitnum.c
@@ -0,0 +1,17 @@
+/*
+Program Name : checkifdigitlessthandigit1ina2digitnum.c
+Author       : Aishwarya V
+Description  : Program about check if digit 0 less than digit 1 are not 
+*/
+
+#include<stdio.h>		//header file
+
+int main()					//main function
+
+{
+	int x;					//declared variable x
+	printf("Enter a number : ");
+	scanf("%d",&x);
+	printf("%d",(x%10<x/10));		//checkifdigitlessthandigit1ina2digitnum
+	return 0;
+}
