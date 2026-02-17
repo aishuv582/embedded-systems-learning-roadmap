d63e05d3ab77286923c98dd0c06bc6395814c99e
aishuv582 <aishuv830@gmail.com>
2026-02-17 07:58:35 +0530

    checkifgivennumisnotequalto50ornot.c

---
+++ checkifgivennumisnotequalto50ornot.c
@@ -0,0 +1,25 @@
+/*
+Program Name : checkifgivennumisnotequalto50.c
+Author       : Aishwarya V
+Description  : Program about check if given num is not equal to 50 
+*/
+
+
+#include<stdio.h>				//header file
+
+int main()					//main function
+
+{
+	int x;						//declared variable x
+	printf("Enter a number : ");			//getting a input from the user
+	scanf("%d",&x);			
+	if(x!=50)						//logic for x!=50
+	{
+		printf("success");
+	}
+	else{
+		printf("Failure");
+	}
+	
+	return 0;
+}
