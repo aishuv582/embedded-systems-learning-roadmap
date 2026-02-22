c83d184712922ee88187c9600c2321c2ae6b0fca
aishuv582 <aishuv830@gmail.com>
2026-02-22 21:24:07 +0530

    checkifdigit0isgreaterthanorequaltodigit1inagivennum.c

---
+++ checkifdigit0isgreaterthanorequaltodigit1inagivennum.c
@@ -0,0 +1,25 @@
+/*
+Program Name : checkifdigit0isgreaterthanorequaltodigit1inagivennum.c
+Author       : Aishwarya V
+Description  : Program about check if digit 0  is greater than or equal to digit 1 in the given num 
+*/
+
+
+#include<stdio.h>				//header file
+
+int main()					//main function
+
+{
+	int x;						//declared variable x
+	printf("Enter a number : ");			//getting a input from the user
+	scanf("%d",&x);			
+	if(x/10>=x%10)			//logic for   if digit 0 is greater than or equal to  digit 1 print success otherwise print failure
+	{
+		printf("success");
+	}
+	else{
+		printf("Failure");
+	}
+	
+	return 0;
+}
